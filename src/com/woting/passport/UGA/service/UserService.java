41fbdaf3f46eac5451c937b1077f88dfc606fd38
wanghuiwhwx <wanghui_whwx@sina.com>
2015-10-23 14:37:46 +0800

    create

---
+++ src/com/woting/passport/UGA/service/UserService.java
@@ -0,0 +1,16 @@
+package com.woting.passport.UGA.service;
+
+import com.spiritdata.framework.UGA.UgaUser;
+import com.spiritdata.framework.UGA.UgaUserService;
+
+public class UserService implements UgaUserService{
+    @Override
+    public <V extends UgaUser> V getUserById(String arg0) {
+        return null;
+    }
+
+    @Override
+    public <V extends UgaUser> V getUserByLoginName(String arg0) {
+        return null;
+    }
+}
---
+++ src/com/woting/passport/login/service/LoginServiceImpl.java
@@ -0,0 +1,30 @@
+package com.woting.passport.login.service;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.spiritdata.framework.UGA.UgaUser;
+import com.spiritdata.framework.component.login.service.LoginService;
+
+public class LoginServiceImpl implements LoginService {
+
+    @Override
+    public Map<String, Object> afterUserLoginOk(UgaUser arg0,
+            HttpServletRequest arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> beforeUserLogin(HttpServletRequest arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> onLogout(HttpServletRequest arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
